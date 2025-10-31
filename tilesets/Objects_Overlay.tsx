0cb514809a59c19e8e8767094c829cb59a7e651b
fengen36-cell <fengen36@gmail.com>
2025-10-31 16:31:09 +0800

    Add files via upload

---
+++ tilesets/Objects_Overlay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Objects_Overlay" tilewidth="32" tileheight="32" tilecount="15" columns="3">
+ <image source="Objects_Overlay.png" width="112" height="176"/>
+</tileset>
---
+++ tilesets/Props_Animated.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Props_Animated" tilewidth="32" tileheight="32" tilecount="14" columns="7">
+ <image source="Props_Animated.png" width="224" height="64"/>
+</tileset>
